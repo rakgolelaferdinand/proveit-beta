0473457077ef2195e01c435ab0aa8b999b84c7e9
rakgolelaferdinand <rakgolelaferdinand@gmail.com>
2026-06-22 22:44:26 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
-  <polygon points="12,2 22,8 22,16 12,22 2,16 2,8" stroke="#00D4C8" stroke-width="2" fill="none"/>
-  <circle cx="12" cy="12" r="2" fill="#00D4C8"/>
-</svg>
